bc7ff99633dfac18972c0392b08d85d061333fac
Leonardo Azocar <leonardoazocar_v16@hotmail.com>
2016-05-11 09:40:23 -0430

    Create Etiquetas.java

---
+++ src/marketvzla/Modelo/Etiquetas.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Leonardo
+ */
+public class Etiquetas {
+    
+    public static final String puerto = "5432";
+    public static final String nombrebd = "marketvenezuela";
+    public static final String contraseña = "12345";
+    
+}
